594637f1bdf3d0335bfdd21e78bce7f738d72063
hgiai456 <162857899+hgiai456@users.noreply.github.com>
2024-09-12 19:35:33 +0700

    First commit

---
+++ QLCoffee/QLCoffee/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using QLCoffee.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QLCoffee.Controllers
+{
+    public class HomeController : Controller
+    {
+        QuanLyQuanCoffeeEntities db = new QuanLyQuanCoffeeEntities();
+        
+        public ActionResult DanhSachSanPham()
+        {
+            var listSP = db.SANPHAMs.ToList();
+
+            return View(listSP);
+        }
+        public ActionResult TrangChu()
+        {
+            return View();
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ QLCoffee/QLCoffee/Controllers/LoginController.cs
@@ -0,0 +1,94 @@
+﻿using QLCoffee.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QLCoffee.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        QuanLyQuanCoffeeEntities database = new QuanLyQuanCoffeeEntities();
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult AuthenLogin(TAIKHOAN taikhoan)
+        {
+            try
+            {
+                var check_ID = database.TAIKHOANs.Where(s => s.TenDN == taikhoan.TenDN).FirstOrDefault();
+                var check_Pass = database.TAIKHOANs.Where(s => s.MatKhau == taikhoan.MatKhau).FirstOrDefault();
+                var user = database.TAIKHOANs.Where(s => s.TenDN == taikhoan.TenDN).FirstOrDefault();
+                if (check_ID == null || check_Pass == null)
+                {
+                    if (check_ID == null)
+                    {
+
+                        ViewBag.ErrorID = "Tên Đăng Nhập Sai";
+
+                    }
+                    if (check_Pass == null)
+                    {
+
+                        ViewBag.ErrorPass = "Mật Khẩu Đăng Nhập Sai";
+
+                    }
+                    return View("Login");
+                }
+                else
+                {
+                    if (user.PhanQuyen == "0")
+                    {
+
+                        Session["TenDN"] = user.TenDN;
+                        Session["PhanQuyen"] = user.PhanQuyen;
+                        return RedirectToAction("Index", "TaiKhoan");
+                    }
+                    else if (user.PhanQuyen == "1")
+                    {
+                        Session["TenDN"] = user.TenDN;
+                        Session["PhanQuyen"] = user.PhanQuyen;
+                        return RedirectToAction("TrangChu", "Home");
+                    }
+                    else
+                    {
+                        ViewBag.ErrorMessage = "SAI rồi Ba";
+                        return View("Login");
+                    }
+                }
+            } 
+            catch
+            {
+                return View("Login");
+            }
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+        public ActionResult AuthenRegister(TAIKHOAN taikhoan)
+        {
+            try
+            {
+                var check_PhanQuyen = database.TAIKHOANs.Where(s => s.PhanQuyen == taikhoan.PhanQuyen).FirstOrDefault();
+                if (check_PhanQuyen == null)
+                {
+                    taikhoan.PhanQuyen = "1";
+                }
+                
+
+                database.TAIKHOANs.Add(taikhoan); 
+                database.SaveChanges();   /*Lưu lại data*/
+                return RedirectToAction("Login"); /*điều hướng về trang login*/
+
+            }
+            catch
+            { 
+                return View("Register") ;
+            }
+        }
+    }
+}
